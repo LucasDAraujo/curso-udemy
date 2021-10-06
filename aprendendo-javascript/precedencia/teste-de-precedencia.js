22060d22026ec60c76b5397e349c4cc102330f14
LucasDAraujo <lukashalan@hotmail.com>
2021-10-05 19:20:52 -0500

    Testei a precedência dos códigos no documento HTML

---
+++ aprendendo-javascript/precedencia/teste-de-precedencia.js
@@ -0,0 +1,9 @@
+alert("Testando precedência no código");
+
+/**
+ * O CÓDIGO ABAIXO VAI
+ * SELECIONAR UM ELEMENTO
+ * NO DOM E ALTERAR O ATRIBUTO
+ * VALUE COM UMA STRING
+ */
+document.getElementById("nome").value = "Oi";
