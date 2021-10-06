7113977480cbc705fa88912ecede546f7defdf7a
LucasDAraujo <lukashalan@hotmail.com>
2021-10-05 19:18:56 -0500

    Utilizei o casting

---
+++ aprendendo-javascript/casting-tipos/script.js
@@ -0,0 +1,11 @@
+// MÉTODOS:
+// parseFloat
+// parseInt
+let var1 = parseInt("1");
+let var2 = parseInt("2");
+let var3 = 35;
+let var4 = 4;
+document.write(var1 + var2+"<br>");
+
+// Concatenar números com o .toString():
+document.write(var3.toString() + "<br>" + var4.toString());
