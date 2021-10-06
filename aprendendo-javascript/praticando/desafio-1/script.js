7f88e5323427f3030c16e2b47b5693c762c94854
LucasDAraujo <lukashalan@hotmail.com>
2021-10-05 19:17:30 -0500

    Fiz o desafio 1, que envolve variáveis

---
+++ aprendendo-javascript/praticando/desafio-1/script.js
@@ -0,0 +1,18 @@
+a = 10
+b = 20
+c = null
+document.write('<h1>Variáveis inicialmente:</h1>')
+document.write('<h2>A:' +a+ '</h2>')
+document.write('<h2>B:' +b+ '</h2>')
+document.write('<h2>C:' +c+ '</h2>')
+document.write('<hr>')
+// INVERTENDO VALORES
+c = a 
+a = b
+b = c
+// INVERTENDO VALORES
+
+document.write('<h1>Variáveis depois:</h1>')
+document.write('<h2>A:' +a+ '</h2>')
+document.write('<h2>B:' +b+ '</h2>')
+document.write('<h2>C:' +c+ '</h2>')
