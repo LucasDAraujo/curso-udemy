64b68ef55ccc2cdfcde13ac05f8d81e030325e1a
LucasDAraujo <lukashalan@hotmail.com>
2021-10-05 19:20:15 -0500

    Utilizei o switch

---
+++ aprendendo-javascript/switch/script.js
@@ -0,0 +1,13 @@
+let parametro = 2;
+
+switch (parametro) {
+    case 1:
+        document.write("Parâmetro 1");
+        break;
+    case 2:
+        document.write("Parâmetro 2");
+        break;
+    default:
+        document.write("Parâmetro inválido");
+        break;
+}
