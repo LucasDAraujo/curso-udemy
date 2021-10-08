5afac8d8291a1aabebb135e2521d937c48c835de
LucasDAraujo <lukashalan@hotmail.com>
2021-10-08 12:01:26 -0500

    Ordenei os vetores

---
+++ aprendendo-javascript/arrays/array-ordenacao/script.js
@@ -0,0 +1,19 @@
+let listaFrutas = ['banana', 'maçã', 'uva', 'abacaxi']
+console.log(listaFrutas);
+
+//Faz uma ordenação alfanumérica(alfabética)
+console.log(listaFrutas.sort());
+
+let listaNumeros = [1]
+listaNumeros[1] = 50
+listaNumeros[2] = 371
+listaNumeros[3] = 03
+listaNumeros[4] = 84
+listaNumeros[5] = 17
+console.log(listaNumeros);
+//console.log(listaNumeros.sort(ordenaNumeros));
+
+array.sort(numberSort)
+function numberSort(a, b) {
+    return a - b
+}
