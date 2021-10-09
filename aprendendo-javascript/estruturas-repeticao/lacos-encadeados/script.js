eb35943c0f987751e7f113ef528e0fa138f93a60
LucasDAraujo <lukashalan@hotmail.com>
2021-10-09 07:43:48 -0500

    Usei alços encadeados

---
+++ aprendendo-javascript/estruturas-repeticao/lacos-encadeados/script.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 10; i++) {
+    document.write(`Tabuada do ${i}<br />`)
+    for (let j = 1; j <= 10; j++) {
+        document.write(`Tabuada do ${i}X${j} = ${i*j} <br />`)
+        
+    }
+    document.write("<br />")
+
+}
