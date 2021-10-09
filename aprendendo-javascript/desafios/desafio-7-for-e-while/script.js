20d6951625978229b89a18b6ff43c4188f25d324
LucasDAraujo <lukashalan@hotmail.com>
2021-10-09 18:05:11 -0500

    Fiz o desafio 7, usando for e while

---
+++ aprendendo-javascript/desafios/desafio-7-for-e-while/script.js
@@ -0,0 +1,12 @@
+let listaFrutas = ['Banana', 'Maçã', 'Morango', 'Uva']
+
+
+for (let i = 0; i < listaFrutas.length; i++) {
+    const fruta = listaFrutas[i];
+    document.write(`${fruta}<br />` )
+    
+}
+let f = function(elemento){
+    /*Lógica*/
+}
+arrayName.forEach(f)
