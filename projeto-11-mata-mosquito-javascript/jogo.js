1c9f6c8176fbe3f77d0bda697d24c0200c1f45b5
LucasDAraujo <lukashalan@hotmail.com>
2021-10-10 10:42:53 -0500

    Configurei a lógica dos pontos de HP

--- projeto-11-mata-mosquito-javascript/jogo.js
+++ projeto-11-mata-mosquito-javascript/jogo.js
@@ -1,55 +1,71 @@
-    /* ---------------------------- REDIMENSIONA JOGO --------------------------- */
+// Vidas do jogador
+let vidas = 1
 
-    //Altura e largura do browser
-    let largura = window.innerWidth
-    let altura = window.innerHeight
+/* ---------------------------- REDIMENSIONA JOGO --------------------------- */
 
-    //Altera altura e largura do browser a cada redimensionamento
-    function redimensionaPalcoJogo() {
-        largura = window.innerWidth
-        altura = window.innerHeight
+//Altura e largura do browser
+let largura = window.innerWidth
+let altura = window.innerHeight
 
-    }
-    /* ---------------------------- REDIMENSIONA JOGO --------------------------- */
+//Altera altura e largura do browser a cada redimensionamento
+function redimensionaPalcoJogo() {
+    largura = window.innerWidth
+    altura = window.innerHeight
 
-    /* ----------------- CRIA POSIÇÕES RANDÔMICAS PROS MOSQUITOS ---------------- */
-    function posicionaRandomicamente() {
+}
+/* ---------------------------- REDIMENSIONA JOGO --------------------------- */
 
-        //Remover o mosquito anterior, caso exista.
-        if (document.getElementById('mosquito')) {
-            document.getElementById('mosquito').remove()
+/* ----------------- FUNÇÕES QUE CONTROLAM OS MOSQUITOS E PONTOS DE HP ---------------- */
+
+function posicionaRandomicamente() {
+
+    //Remover o mosquito anterior, caso exista, ou caso o usuário não clique.
+    if (document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+
+        //Quando você remove o mosquito automaticamente, subtrai 1 ponto de HP do player
+        if (vidas > 3) {
+            alert('Interrompeu o jogo')
+        } else {
+            document.getElementById(`vida-${vidas}`).src = 'images/coracao_vazio.png'
+            vidas++
         }
-
-
-        let posicaoX = Math.floor(Math.random() * window.innerWidth) - 90
-        let posicaoY = Math.floor(Math.random() * window.innerHeight) - 90
-        posicaoX < 0 ? posicaoX = 20 : posicaoX
-        posicaoY < 0 ? posicaoY = 20 : posicaoY
-
-        //Criar o elemento html 
-        let imagemMosquito = document.createElement('img')
-        imagemMosquito.src = 'images/mosca.png'
-        imagemMosquito.id = 'mosquito'
-
-        //Dá uma classe css aleatória baseada no retorno da função(tamanhos e lados)
-        imagemMosquito.className = `mosquito-${tamanhoAleatorioMosquitos()}`
-        imagemMosquito.className += ` lado-${ladoAleatorio()}`
-
-        //Define a posição do inseto
-        imagemMosquito.style.left = `${posicaoX}px`
-        imagemMosquito.style.top = `${posicaoY}px`
-        document.body.appendChild(imagemMosquito)
-    }
-    /* ----------------- CRIA POSIÇÕES RANDÔMICAS PROS MOSQUITOS --------------- */
-
-
-    /* ---------- CRIA TAMANHOS/DIREÇÕES ALEATÓRIOS PRAS IMAGENS DOS MOSQUITOS ---------- */
-    function tamanhoAleatorioMosquitos() {
-        return Math.ceil(Math.random() * 3)
     }
 
-    function ladoAleatorio() {
-        return Math.ceil(Math.random() * 2)
 
+    let posicaoX = Math.ceil(Math.random() * window.innerWidth) - 90
+    let posicaoY = Math.ceil(Math.random() * window.innerHeight) - 90
+    posicaoX < 0 ? posicaoX = 20 : posicaoX
+    posicaoY < 0 ? posicaoY = 20 : posicaoY
+
+    //Criar o elemento html 
+    let imagemMosquito = document.createElement('img')
+    imagemMosquito.src = 'images/mosca.png'
+    imagemMosquito.id = 'mosquito'
+    imagemMosquito.draggable = false
+    imagemMosquito.onclick = function () {
+        this.remove()
     }
-    /* ---------- CRIA TAMANHOS/DIREÇÕES ALEATÓRIOS PRAS IMAGENS DOS MOSQUITOS ---------- */
+
+    //Dá uma classe css aleatória baseada no retorno da função(tamanhos e lados)
+    imagemMosquito.className = `mosquito-${tamanhoAleatorioMosquitos()}`
+    imagemMosquito.className += ` lado-${ladoAleatorio()}`
+
+    //Define a posição do inseto
+    imagemMosquito.style.left = `${posicaoX}px`
+    imagemMosquito.style.top = `${posicaoY}px`
+    document.body.appendChild(imagemMosquito)
+}
+/* ----------------- FUNÇÕES QUE CONTROLAM OS MOSQUITOS E PONTOS DE HP --------------- */
+
+
+/* ---------- CRIA TAMANHOS/DIREÇÕES ALEATÓRIOS PRAS IMAGENS DOS MOSQUITOS ---------- */
+function tamanhoAleatorioMosquitos() {
+    return Math.ceil(Math.random() * 3)
+}
+
+function ladoAleatorio() {
+    return Math.ceil(Math.random() * 2)
+
+}
+/* ---------- CRIA TAMANHOS/DIREÇÕES ALEATÓRIOS PRAS IMAGENS DOS MOSQUITOS ---------- */
