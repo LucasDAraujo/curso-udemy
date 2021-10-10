fa8b82f453e19b162ee12a485abd940bc2d7f8f7
LucasDAraujo <lukashalan@hotmail.com>
2021-10-09 20:52:41 -0500

    Programei a função de setar tamanho, lado e posição dos mosquitos

---
+++ projeto-11-mata-mosquito-javascript/jogo.js
@@ -0,0 +1,45 @@
+    /* ---------------------------- REDIMENSIONA JOGO --------------------------- */
+    //Altura e largura do browser
+    let largura = window.innerWidth
+    let altura = window.innerHeight
+
+    //Altera altura e largura do browser a cada redimensionamento
+    function redimensionaPalcoJogo() {
+        largura = window.innerWidth
+        altura = window.innerHeight
+
+    }
+    /* ---------------------------- REDIMENSIONA JOGO --------------------------- */
+
+    /* ----------------- CRIA POSIÇÕES RANDÔMICAS PROS MOSQUITOS ---------------- */
+    let posicaoX = Math.floor(Math.random() * window.innerWidth) - 90
+    let posicaoY = Math.floor(Math.random() * window.innerHeight) - 90
+    posicaoX < 0 ? posicaoX = 20 : posicaoX
+    posicaoY < 0 ? posicaoY = 20 : posicaoY
+    // console.log(posicaoX, posicaoY);
+
+    //Criar o elemento html 
+    let imagemMosquito = document.createElement('img')
+    imagemMosquito.src = 'images/mosca.png'
+
+    //Dá uma classe css aleatória baseada no retorno da função(tamanhos e lados)
+    imagemMosquito.className = `mosquito-${tamanhoAleatorioMosquitos()}`
+    imagemMosquito.className += ` lado-${ladoAleatorio()}`
+
+    //Define a posição do inseto
+    imagemMosquito.style.left = `${posicaoX}px`
+    imagemMosquito.style.top = `${posicaoY}px`
+    document.body.appendChild(imagemMosquito)
+
+    /* ----------------- CRIA POSIÇÕES RANDÔMICAS PROS MOSQUITOS --------------- */
+
+
+    /* ---------- CRIA TAMANHOS/DIREÇÕES ALEATÓRIOS PRAS IMAGENS DOS MOSQUITOS ---------- */
+    function tamanhoAleatorioMosquitos() {
+        return Math.floor(Math.random() * 3) + 1
+    }
+
+    function ladoAleatorio() {
+        return Math.floor(Math.random() * 2) + 1
+    }
+    /* ---------- CRIA TAMANHOS/DIREÇÕES ALEATÓRIOS PRAS IMAGENS DOS MOSQUITOS ---------- */
