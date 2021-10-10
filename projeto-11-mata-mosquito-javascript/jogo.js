39568d2878e1c23a518bc683cef0de7c045a1b79
LucasDAraujo <lukashalan@hotmail.com>
2021-10-10 09:40:57 -0500

    Fiz com que aparecesse apenas um mosquito na tela

--- projeto-11-mata-mosquito-javascript/jogo.js
+++ projeto-11-mata-mosquito-javascript/jogo.js
@@ -1,4 +1,5 @@
     /* ---------------------------- REDIMENSIONA JOGO --------------------------- */
+
     //Altura e largura do browser
     let largura = window.innerWidth
     let altura = window.innerHeight
@@ -12,34 +13,43 @@
     /* ---------------------------- REDIMENSIONA JOGO --------------------------- */
 
     /* ----------------- CRIA POSIÇÕES RANDÔMICAS PROS MOSQUITOS ---------------- */
-    let posicaoX = Math.floor(Math.random() * window.innerWidth) - 90
-    let posicaoY = Math.floor(Math.random() * window.innerHeight) - 90
-    posicaoX < 0 ? posicaoX = 20 : posicaoX
-    posicaoY < 0 ? posicaoY = 20 : posicaoY
-    // console.log(posicaoX, posicaoY);
+    function posicionaRandomicamente() {
 
-    //Criar o elemento html 
-    let imagemMosquito = document.createElement('img')
-    imagemMosquito.src = 'images/mosca.png'
+        //Remover o mosquito anterior, caso exista.
+        if (document.getElementById('mosquito')) {
+            document.getElementById('mosquito').remove()
+        }
 
-    //Dá uma classe css aleatória baseada no retorno da função(tamanhos e lados)
-    imagemMosquito.className = `mosquito-${tamanhoAleatorioMosquitos()}`
-    imagemMosquito.className += ` lado-${ladoAleatorio()}`
 
-    //Define a posição do inseto
-    imagemMosquito.style.left = `${posicaoX}px`
-    imagemMosquito.style.top = `${posicaoY}px`
-    document.body.appendChild(imagemMosquito)
+        let posicaoX = Math.floor(Math.random() * window.innerWidth) - 90
+        let posicaoY = Math.floor(Math.random() * window.innerHeight) - 90
+        posicaoX < 0 ? posicaoX = 20 : posicaoX
+        posicaoY < 0 ? posicaoY = 20 : posicaoY
 
+        //Criar o elemento html 
+        let imagemMosquito = document.createElement('img')
+        imagemMosquito.src = 'images/mosca.png'
+        imagemMosquito.id = 'mosquito'
+
+        //Dá uma classe css aleatória baseada no retorno da função(tamanhos e lados)
+        imagemMosquito.className = `mosquito-${tamanhoAleatorioMosquitos()}`
+        imagemMosquito.className += ` lado-${ladoAleatorio()}`
+
+        //Define a posição do inseto
+        imagemMosquito.style.left = `${posicaoX}px`
+        imagemMosquito.style.top = `${posicaoY}px`
+        document.body.appendChild(imagemMosquito)
+    }
     /* ----------------- CRIA POSIÇÕES RANDÔMICAS PROS MOSQUITOS --------------- */
 
 
     /* ---------- CRIA TAMANHOS/DIREÇÕES ALEATÓRIOS PRAS IMAGENS DOS MOSQUITOS ---------- */
     function tamanhoAleatorioMosquitos() {
-        return Math.floor(Math.random() * 3) + 1
+        return Math.ceil(Math.random() * 3)
     }
 
     function ladoAleatorio() {
-        return Math.floor(Math.random() * 2) + 1
+        return Math.ceil(Math.random() * 2)
+
     }
     /* ---------- CRIA TAMANHOS/DIREÇÕES ALEATÓRIOS PRAS IMAGENS DOS MOSQUITOS ---------- */
